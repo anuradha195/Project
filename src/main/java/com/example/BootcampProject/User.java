a76eec128f8a20f537eb6c797b720c0937b61aae
Marita Lasmane <marita.lasmane@gmail.com>
2019-06-26 19:00:55 +0300

    Updates in User class

--- src/main/java/com/example/BootcampProject/User.java
+++ src/main/java/com/example/BootcampProject/User.java
@@ -1,4 +1,31 @@
 package com.example.BootcampProject;
 
 public class User {
+    private String username;
+    private String password;
+    private String pwdConfirm;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPwdConfirm() {
+        return pwdConfirm;
+    }
+
+    public void setPwdConfirm(String pwdConfirm) {
+        this.pwdConfirm = pwdConfirm;
+    }
 }
