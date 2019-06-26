675bdd69e9273642526a5461cc4b17db4b0906ec
Marita Lasmane <marita.lasmane@gmail.com>
2019-06-26 11:43:24 +0300

    Added some ToDos

--- src/main/java/com/example/BootcampProject/DBRequests.java
+++ src/main/java/com/example/BootcampProject/DBRequests.java
@@ -1,4 +1,8 @@
 package com.example.BootcampProject;
 
 public class DBRequests {
+    /*TODO:
+    -create new request for Google Fit
+    -parse data received from Google
+     */
 }
--- src/main/java/com/example/BootcampProject/Database.java
+++ src/main/java/com/example/BootcampProject/Database.java
@@ -1,4 +1,12 @@
 package com.example.BootcampProject;
 
+import java.sql.Connection;
+
 public class Database {
+    /*-TODO:
+    -check user in the database
+    -check password in the database
+    -register new user in the db
+     */
+
 }
